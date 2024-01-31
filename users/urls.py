37f3f4000632fe298af683c6adc2568f42ef0d5f
Ali <ali.nakhaee.kooshe@gmail.com>
2024-01-31 11:35:21 +0330

    register page added

--- users/urls.py
+++ users/urls.py
@@ -2,10 +2,14 @@
 
 from django.urls import path, include
 from django.contrib.auth import views as auth_views
+from . import views
 
 app_name = 'users'
 urlpatterns = [
     # Include default auth urls.
     path('', include('django.contrib.auth.urls')),
+    # Include logout url from default auth.
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
+    # Registration page.
+    path('register/', views.register, name='register'),
 ]
